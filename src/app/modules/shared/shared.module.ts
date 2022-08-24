5357c16611e569af66fbbe21c6c8ffed0bac2f8a
Maye Edwin <mayedwine1@gmail.com>
2022-08-24 14:45:52 +0300

    Setup app

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [SidebarComponent, HeaderComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}

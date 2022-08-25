668851c5ca2706708b88a656ff7c028cc4f77b11
Maye Edwin <mayedwine1@gmail.com>
2022-08-25 20:37:23 +0300

    Updated

--- src/app/modules/shared/components/sidebar/sidebar.component.ts
+++ src/app/modules/shared/components/sidebar/sidebar.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  // Properties.
+  navlist = [
+    {
+      name: 'Home',
+      icon: 'home',
+    },
+    {
+      name: 'Transfer',
+      icon: 'transfer',
+    },
+    {
+      name: 'History',
+      icon: 'history',
+    },
+    {
+      name: 'Profile',
+      icon: 'profile',
+    },
+    {
+      name: 'Settings',
+      icon: 'settings',
+    },
+    {
+      name: 'Billing',
+      icon: 'billing',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

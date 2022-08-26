20dcc5d603cff5c830953b8a9ea8c93b09b24eef
Maye Edwin <mayedwine1@gmail.com>
2022-08-26 21:20:39 +0300

    Complete homepage v1, desktop v1

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  // Properties.
+  actions = [
+    {
+      name: 'Top Up',
+      icon: 'topup',
+    },
+    {
+      name: 'Send Money',
+      icon: 'send',
+    },
+    {
+      name: 'Collect',
+      icon: 'collect',
+    },
+    {
+      name: 'Withdraw',
+      icon: 'withdraw',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 @NgModule({
-  declarations: [SidebarComponent, HeaderComponent],
+  declarations: [SidebarComponent, HeaderComponent, TransactionsComponent],
   imports: [CommonModule, RouterModule],
-  exports: [SidebarComponent, HeaderComponent],
+  exports: [SidebarComponent, HeaderComponent, TransactionsComponent],
 })
 export class SharedModule {}
